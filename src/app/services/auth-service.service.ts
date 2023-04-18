fb291c99b445c0c5e3b7613258a210868193feb0
ntokozo-nhlangulela <nnhlangulela@jhb.dvt.co.za>
2023-04-18 09:58:03 +0200

    refactor: removed commented code for sign-in and sign-up components.

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -19,24 +19,24 @@ export class AuthServiceService {
   // Sign in with email/password
   signIn(email: string, password: string) {
     return signInWithEmailAndPassword(this.authentication, email, password)
-      //change to homepage.
+      //ToDo add route to change to homepage.
       .catch(()=>{
-        //return window.alert('Oops an error has occured');
+        return window.alert('Oops an error has occured');
       })
   }
   // Sign up with email/password
   signUp(email: string, password: string) {
     return createUserWithEmailAndPassword(this.authentication, email, password)
-      //.then(() => this.router.navigate([''])) //change to login
+      .then(() => this.router.navigate(['sign-in'])) //change to login
       .catch(() => {
-        //return window.alert('Oops an error has occured');
+        return window.alert('Oops an error has occured');
       })
   }
   // Sign out
   SignOut() {
     signOut(this.authentication)
       .then(()=>{
-        //this.router.navigate(['sign-in'])
+        this.router.navigate(['sign-in'])
       })
   }
 }
