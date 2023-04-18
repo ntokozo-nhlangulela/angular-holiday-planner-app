d86318edba866788a97ede979f5aa6924b4cb586
ntokozo-nhlangulela <nnhlangulela@jhb.dvt.co.za>
2023-04-18 02:32:09 +0200

    feat/calender: Adding a calender to show user trips once trips are added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CalenderComponent} from "./components/calender/calender.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'calender',
+    component: CalenderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
